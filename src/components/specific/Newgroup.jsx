3c0d5cb39fab62ace95426b25663b0f6727ae452
Ayan <ayan2572006@gmail.com>
2024-09-29 10:24:04 +0530

    new group

--- src/components/specific/Newgroup.jsx
+++ src/components/specific/Newgroup.jsx
@@ -1,4 +1,53 @@
-const Newgroup = () => {
-  return <div>Newgroup</div>;
+import { useInputValidation } from "6pp";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  Skeleton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { sampleUsers } from "../../constants/sampleData";
+import UserItem from "../shared/UserItem";
+
+const NewGroup = () => {
+  const groupName = useInputValidation("");
+  const selectMember = () => {};
+  const onSubmitHandler = () => {};
+  return (
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "3rem" }} width={"25rem"} spacing={"2rem"}>
+        <DialogTitle textAlign={"center"} variant="h4">
+          New Group
+        </DialogTitle>
+
+        <TextField
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+        />
+
+        <Typography variant="body1">Members</Typography>
+
+        <Stack>
+          {sampleUsers.map((i) => (
+            <UserItem user={i} key={i._id} handler={selectMember} />
+          ))}
+        </Stack>
+
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <Button variant="text" color="error" size="large">
+            Cancel
+          </Button>
+          <Button variant="contained" size="large" onClick={onSubmitHandler}>
+            Create
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
 };
-export default Newgroup;
+
+export default NewGroup;
